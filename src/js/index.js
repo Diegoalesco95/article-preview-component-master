2cc8ffbaa65baf22bc548bfa47a89d66ad0524b3
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-08-04 18:21:19 -0500

    Update animations & active share options

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,20 @@
 //  handle for button
 const handleClick = () => {
-  const actions = document.querySelector('.card__actions');
-  const elements = document.querySelectorAll('.avatar, .information');
-  const shared = document.querySelector('.shared-options');
-  if (shared.style.display === 'flex') {
-    shared.style.display = '';
-    actions.classList.remove('active');
-    elements.forEach((elm) => {
-      elm.style.display = '';
-    });
+  const shared = document.querySelector('.card__actions--shared');
+  const isActive = shared.classList.contains('active');
+  if (isActive) {
+    button.style.backgroundColor = '';
+    button.style.color = '';
+    shared.style.animation = '0.3s shareClose forwards';
+    shared.classList.remove('active');
   } else {
-    shared.style.display = 'flex';
-    actions.classList.add('active');
-    elements.forEach((elm) => {
-      elm.style.display = 'none';
-    });
+    button.style.backgroundColor = '#6d7f97';
+    button.style.color = '#ecf2f8';
+    shared.style.animation = '0.3s shareOpen forwards';
+    shared.classList.add('active');
   }
 };
 
 // Add Listeners
-document.querySelector('.share-button').addEventListener('click', handleClick);
+const button = document.querySelector('.share-button');
+const shareButton = button.addEventListener('click', handleClick);
